5c198ef89525faaf7a599aacbf45198e39bfb1a6
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-05-27 13:07:16 -0400

    collision detection, ball stays in canvas

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 var canvas = document.getElementById('mycanvas');
 var ctx = canvas.getContext('2d');
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height -30;
 var dx = 2;
 var dy = -2;
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+function drawBall() {
     ctx.beginPath();
     // Create the ball
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx
+    } 
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy
+    }
     x +=dx;
     y +=dy;
 }
