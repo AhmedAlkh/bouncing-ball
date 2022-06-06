cd7236aa07eb5bc8c230320f5873a6a32b75a6a5
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-06-06 11:52:25 -0400

    implementing collision detection

--- index.js
+++ index.js
@@ -82,6 +82,20 @@ function drawBricks() {
     }
 }
 
+function collisionDetection() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
