34ad46af18ff1281ce375bb1c7cce554eb9527c6
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-06-06 11:19:06 -0400

    created functions to control events

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ var leftPressed = false;
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
